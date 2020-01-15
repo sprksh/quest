149b93c9ac769b7476f088d26db413665a02d938
Surya Prakash <sprksh.j@gmail.com>
2020-01-15 12:18:03 +0530

    all arrys of n length using recursion

---
+++ recursion/backtracking.py
@@ -0,0 +1,64 @@
+class newNode:
+
+    # Construct to create a new node
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.parent = None
+
+    def __repr__(self):
+        return str(self.key)
+
+def n_queens(n):
+    # backtracking
+    pass
+
+
+def tree_sum(root):
+    if root is None:
+        return 0
+    return root.key + tree_sum(root.left) + tree_sum(root.right)
+
+
+def max_path_sum(root):
+    # base case
+    def sub_tree_sum(node):
+        if node is None:
+            return 0
+        l = sub_tree_sum(node.left)
+        r = sub_tree_sum(node.right)
+        return node.key + l if l > r else node.key + r
+
+    max_path_sum = sub_tree_sum(root)
+    return max_path_sum
+
+
+def max_sum_path(root):
+    # needs to return path, no idea how
+    # this page contains a solution but try
+    # https://www.techiedelight.com/find-maximum-sum-root-to-leaf-path-binary-tree/
+    def sub_tree_sum(node):
+        if node is None:
+            return 0
+        l = sub_tree_sum(node.left)
+        r = sub_tree_sum(node.right)
+        return node.key + l if l > r else node.key + r
+
+    max_path_sum = sub_tree_sum(root)
+    return max_path_sum
+
+
+if __name__ == '__main__':
+    root = newNode(1)
+    root.left = newNode(2)
+    root.right = newNode(3)
+    root.left.left = newNode(4)
+    root.left.right = newNode(5)
+    root.right.left = newNode(6)
+    root.right.right = newNode(7)
+    root.right.left.right = newNode(12)
+
+    sum = max_path_sum(root)
+
+    print(sum)
--- recursion/n_queens.py
+++ recursion/n_queens.py
@@ -1,59 +1,104 @@
-class newNode:
+class NQueens:
+    """
+    nxn chessboard
+    solution array:
+    it will contain n elements corresponding to n rows.
+    each element will be an integer between 0 - n-1 as the column index of where the queen is
 
-    # Construct to create a new node
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
+    # [1,3,0,2]
+    """
+    def __init__(self):
+        self.all_solutions = []
 
-    def __repr__(self):
-        return str(self.key)
+    def solve_iteratively(self, n):
+        solution = False
+        solution_arrays = []
+        solution_array = [None for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                solution_array[j] = i
+                s = solution_array.copy()
 
-def n_queens(n):
-    # backtracking
-    pass
+                solution_arrays.append(s)
+        print(solution_arrays)
+        print(len(solution_arrays))
+
+        # solution_arrays = [[1,3,0,2]]
+
+        for s in solution_arrays:
+            fits = self.check_if_all_queen_fits_conditions(s, n)
+            print(str(fits))
+            if fits:
+                self.all_solutions.append(s)
+        return self.all_solutions
+
+    def solve_iteratively_better(self):
+        pass
+
+    def solve_recursively(self):
+        pass
+
+    def check_if_all_queen_fits_conditions(self, solution_array, n):
+        for row, column in enumerate(solution_array):
+            # here i is row, j is column
+            # condition is:if self.solution_array[row] == column, return false
+            print("\nFor solution array %s" % str(solution_array))
+
+            print(str([row, column]))
+            print("We checked")
+
+            if column in solution_array:
+                return False
+
+            # check upper diagonal left
+            for i in range(min(row, column), -1, -1):
+                new_row, new_column = row + i, column - i
+                print(str([new_row, new_column]), end=' ')
+                if new_row == -1 or new_column == -1:
+                    return True
+                if solution_array[new_row] == new_column:
+                    return False
+
+            print("\nAnd")
+            # check lower diagonal left
+            for i in range(min(row, column), n):
+                new_row, new_column = row - i, column - i
+                print(str([new_row, new_column]), end=' ')
+                if new_row == -1 or new_column == -1:
+                    return True
+                if solution_array[new_row] == new_column:
+                    return False
+            return True
+
+    def create_arrays(self, n):
+        arrays = []
+        crr = [None for _ in range(n)]
+        def create(crr, arr, a=0):
+            print(crr, arr, a)
+            if a == n:
+                arrays.append(crr.copy())
+                return crr
+            for i in arr:
+                p = arr.pop(i)
+                crr[a] = p
+                crr = create(crr, arr, a+1)
+        create(crr, list(range(n)))
+        return arrays
+
+    def create_1(self, n):
+        crr = list(range(n))
+        def r():
+            for i in crr:
+                arr = crr.copy()
+                p = arr.pop(i)
 
 
-def tree_sum(root):
-    if root is None:
-        return 0
-    return root.key + tree_sum(root.left) + tree_sum(root.right)
 
 
 
-
-
-def max_path_sum(root):
-    max_sum_path = [root]
-
-    # base case
-    def traverse(node):
-        if node is None:
-            return 0
-
-        l = traverse(node.left)
-        r = traverse(node.right)
-        if l > r:
-            max_sum_path.append(node.left)
-            return node.key + l
-        if r > l:
-            max_sum_path.append(node.right)
-            return node.key + r
-
-    max_path_sum = traverse(root)
-    return (max_path_sum, max_sum_path)
-
-
-if __name__ == '__main__':
-    root = newNode(1)
-    root.left = newNode(2)
-    root.right = newNode(3)
-    root.left.left = newNode(4)
-    root.left.right = newNode(5)
-    root.right.left = newNode(6)
-    root.right.right = newNode(7)
-    root.right.left.right = newNode(8)
-
-    sum = max_path_sum(root)
-
-    print(sum)
+if __name__ == "__main__":
+    # solution = NQueens().solve_iteratively(4)
+    # print(solution)
+    arrays = NQueens().create_arrays(4)
+    print(len(arrays))
+    print(arrays)
