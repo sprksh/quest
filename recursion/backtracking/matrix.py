e6d2b1f359265700e0cf0dff15cd45d053bc4243
Surya Prakash <sprksh.j@gmail.com>
2020-01-18 18:49:42 +0530

    matrix path backtracking solution

--- recursion/backtracking/matrix.py
+++ recursion/backtracking/matrix.py
@@ -1,7 +1,42 @@
 class Matrix:
-    def __init__(self, n):
+    def __init__(self, n, destination):
         self.n = n
-        self.matrix = [[0 for _ in n] for _ in n]
+        self.visited_matrix = [[0 for _ in range(n)] for _ in range(n)]
+        self.visited_matrix[0][0] = 1
+        self.destination = destination
 
-    def visit(self):
-        pass
+    def visit(self, matrix, coordinates=(0,0), path=[(0,0)]):
+        if coordinates == self.destination:
+            print(path)
+            return path
+
+        else:
+            options = [(0,1), (0,-1), (-1,0), (1,0)]
+            for o in options:
+                new_coordinates = (coordinates[0] + o[0], coordinates[1] + o[1])
+                x, y = new_coordinates[0], new_coordinates[1]
+                if 0 <= x < self.n and 0 <= y < self.n and self.visited_matrix[x][y] == 0 and matrix[x][y] == 1:
+                    self.visited_matrix[x][y] = 1
+                    path.append((x,y))
+                    self.visit(matrix, (x,y), path)
+                    # again below lines backtrack
+                    path.pop()
+                    self.visited_matrix[x][y] = 0
+
+
+if __name__ == "__main__":
+    path_matrix = [
+        [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+        [1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
+        [1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
+        [0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+        [1, 0, 1, 1, 1, 1, 0, 0, 1, 1],
+        [1, 1, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 0, 1, 0, 0]
+    ]
+    destination = (5,7)
+    path = Matrix(10, destination).visit(path_matrix)
+    print(path)
